8009d0db3b9d45c9cd4e43169394fe08b4b673c4
ER1CK122 <erckgames53@gmail.com>
2022-11-21 17:28:11 -0300

    Todo list

--- src/pages/sandbox.jsx
+++ src/pages/sandbox.jsx
@@ -8,7 +8,7 @@ export default function Sandbox(){
       <main className='d-flex flex-column align-items-center justify-content-center vh-100 container'>
         <div className="d-flex mb-3">
           <form action="" className='me-2'>
-            <input type="text" className="ps-2 rounded form-control"/>c
+            <input type="text" className="ps-2 rounded form-control"/>
 
           </form>
           <button className="btn btn-outline-primary" onClick={()=>{}}>Adicionar</button>
