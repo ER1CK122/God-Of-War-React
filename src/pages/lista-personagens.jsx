ca0cbe6ba1d89fa83ec88294c55d16d2e7bfe570
ER1CK122 <erckgames53@gmail.com>
2022-12-01 17:18:18 -0300

    problema com a URL localhost

--- src/pages/lista-personagens.jsx
+++ src/pages/lista-personagens.jsx
@@ -1,43 +1,23 @@
-import React, {useEffect, useState} from "react";
 import axios from "axios";
+import React from "react";
 
 export default function ListaPersonagens(){
-  const [personagens, setPersonagens] = useState([]);
+  const [personagens, setPersonagens] = React.useState();
 
-  const getPersonagens = async () => {
-    try {
-      const response = await axios.get("http://localhost/GOD-OF-WAR-BACK-END/api/lista-personagem.php");
-      const data = response;
-      setPersonagens(data);
-      console.log(data)
-    }  catch (error) {
-      console.log(error)
-    }
-  }
-  useEffect(() => {
-    getPersonagens();
-  },[])
-
-
-  let aPersonagens = []
-    useEffect(() => {
-      axios("http://localhost/GOD-OF-WAR-BACK-END/api/lista-personagem.php")
-      .then((resp) => {resp.json()})
-      .then((data) => {  
-        console.log(data)
-      }).catch((err) => console.log(err));   
-    },[])
-
+  React.useEffect(()=>{
+    axios.get("http://192.168.0.27:80/GOD-OF-WAR-BACK-END/api/lista-personagem.php")
+    .then((response) => {
+      setPersonagens(response.data);
+    });
+  },[]);
 
     return (
-      <>
-        <div>
-teste
-          {personagens.map((personagem) => {
-            <div>{personagem.nomePersonagem}</div>
-          })}
-
-        </div>
-      </>
+      <div>
+        teste
+        <div>{personagens?.id}</div>
+        <div>{personagens?.nomePersonagem}</div>
+        <div>{personagens?.mitologia}</div>
+        <div>{personagens?.nomeMitologia}</div>
+      </div>
     )
 }
